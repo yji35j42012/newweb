97133a4362925c3d4fcede887aacb0b06050ebe5
yji35j42012 <yji35j42012@gmail.com>
2020-11-11 18:16:54 +0800

    109.11.11 問題修正 1.首頁背景圖先淡入 霧氣跑兩秒 態度再出來 2.mini 文字16px 3.產品服務高度加大. 後面三角形顏色 4.圓形粒子 中心變黃 5.電腦影子 6.loading 縮小問題 服務項目流程 增加流星動畫 按鈕顏色調整 刪除未用到圖片

--- script/index.js
+++ script/index.js
@@ -1,116 +1,159 @@
-var move = 0
-var list = $('.banner_group>div').length
-var banneStart;
-var bannePlay = true
+//  load讀取條 
 
-$(document).ready(function(){
-    bannePlay = true
-    $(`.banner_group>div:eq(0)`).addClass('active') 
-    if(bannePlay){goPlay();} 
-})	
-$('#move_about').mouseenter(function(evt){
-    bannePlay = false;
-    clearInterval(banneStart);
-})
-$('#move_about').mouseleave(function(e) {
-    bannePlay = true
-    if(bannePlay){goPlay();}
-});
+  var nowLoad = 0
+  var imgLength = $('img').length
+   $('img').each(function(){
+       var x = $('img').index(this)
+        $(this).ready(function(){
+            nowLoad += 100 / imgLength
+            $('.loading_box span').animate({'left':`${parseInt(nowLoad)}%`},80);
+        })
+   })
+
+   var barWidth = $('.loading_box').css('width')
+   var barTime = setInterval(() => {
+       var barLeft =  $('.loading_box span').css('left') 
+       var now = parseInt(parseFloat((parseInt(barLeft) / parseInt(barWidth))*102))  
+       $('.loading tt').text(now)
+       if(now >= 100){
+           clearInterval(barTime)
+           $('#load').fadeOut(1000);
+       }
+   },10);
 
 
-//禁止圖片拖動
-$('#move_about img').mousedown(function(e){
-    e.preventDefault()
-}) 
-var downX,upX; 
-$('#move_about').mousedown(function(e){
-   downX = e.clientX
-})
-$('#move_about').mouseup(function(e){
-    upX = e.clientX
-   if(downX - upX > 100){
+ // 登入彈窗控制
+   var signIn = false;
+   $('.menu li:eq(7)').click(function(){
+        signIn = true;
+        nowSignIn()
+   })
+
+   $('.login_close_btn').click(function(){
+        signIn = false;
+        nowSignIn()
+   })
+   function nowSignIn(){
+     if(signIn){ $('#signIn').fadeIn(500)}
+     else{ $('#signIn').fadeOut(500)} 
+   }
+
+
+    
+    var move = 0
+    var list = $('.banner_group>div').length
+    var banneStart;
+    var bannePlay = true
+
+    $(document).ready(function(){
+        bannePlay = true
+        $(`.banner_group>div:eq(0)`).addClass('active') 
+        if(bannePlay){goPlay();} 
+    })	
+    $('#move_about').mouseenter(function(evt){
+        bannePlay = false;
+        clearInterval(banneStart);
+    })
+    $('#move_about').mouseleave(function(e) {
+        bannePlay = true
+        if(bannePlay){goPlay();}
+    });
+
+
+    //禁止圖片拖動
+    $('#move_about img').mousedown(function(e){
+        e.preventDefault()
+    }) 
+    var downX,upX; 
+    $('#move_about').mousedown(function(e){
+       downX = e.clientX
+    })
+    $('#move_about').mouseup(function(e){
+        upX = e.clientX
+       if(downX - upX > 100){
+            move == list - 1? move = 1 : move++
+            $('.banner_dots li').removeClass('on')
+		    if(move>0 && move < list - 1){action_1()}
+            else if(move == list - 1){action_2()} 
+       }
+     })
+
+    // 輪播計時器
+    function goPlay() {
+        if(bannePlay){
+            banneStart = setInterval(banner, 10000)
+        }      
+    };
+
+    function banner(){
         move == list - 1? move = 1 : move++
         $('.banner_dots li').removeClass('on')
-        if(move>0 && move < list - 1){action_1()}
-        else if(move == list - 1){action_2()} 
-   }
- })
-
-// 輪播計時器
-function goPlay() {
-    if(bannePlay){
-        banneStart = setInterval(banner, 10000)
-    }      
-};
-
-function banner(){
-    move == list - 1? move = 1 : move++
-    $('.banner_dots li').removeClass('on')
-    if(move>0 && move < list - 1){action_1()}
-    else if(move == list - 1){action_2()}     
-}
-
-// 輪播按鍵點擊事件
-$('.banner_dots li').click(function(){
-    bannePlay = false;
-
-    console.log(bannePlay)
-    $('.banner_dots li').removeClass('on')
-    var find = $('.banner_dots li').index(this)
-    if(move < 3){
-        move = find;
-        if(find>0 && find < list - 1){
-            move = find;
-            action_1();
-        }
-        else if(find == 0){      
-            action_2()
-        }	
-    } 
-    else if(move == 3){
-        if(find>0 && find < list - 1){
-            move = find;
-            action_1();
-        }
-        else if(find == 0){      
-            move = 4
-            action_2()
-        }	   
+		if(move>0 && move < list - 1){action_1()}
+        else if(move == list - 1){action_2()}     
     }
-})
 
-// ====== 函式區塊 ======
-function goTransform(page,time){
-    $('.banner_group').css('transform',`translateX(-${page * 100}%)`).css('transition',`${time}s`);
-}
-function changeClass(name){
-    $(`.banner_group>div`).removeClass('active')
-    $(`.banner_group>div:eq(${name-1})`).addClass('active')
-}
-function action_1(){
-    goTransform(move, 0.5);
-    setTimeout(function(){changeClass(move+1)},500);
-    $(`.banner_dots li:eq(${move})`).addClass('on')
-}
-function action_2(){
-    goTransform(move, 0.5);
-    setTimeout(function(){goTransform(0, 0);},500);
-    setTimeout(function(){
-        changeClass(1)
-        move = 0
-    },510)
-    $(`.banner_dots li:eq(0)`).addClass('on')
-}
+    // 輪播按鍵點擊事件
+    $('.banner_dots li').click(function(){
+        bannePlay = false;
 
-
-// 漢堡bar點擊menu 展開/關閉
-$('.menu_btn').click(function(){$(this).toggleClass('active')})
-$('.menu li').click(function(){$('.menu_btn').removeClass('active')})
-
-var scroll = $(window).scroll(function () {
-    for (var i = 1; i < 4; i++) {
-        if (scroll.scrollTop() > $(`#hm_${i}`).offset().top + $(`#hm_${i}`).height() / 3 * 2) {
-            $(`#hm_${i + 1}`).addClass('active')
+        console.log(bannePlay)
+        $('.banner_dots li').removeClass('on')
+        var find = $('.banner_dots li').index(this)
+        if(move < 3){
+            move = find;
+            if(find>0 && find < list - 1){
+                move = find;
+                action_1();
+            }
+            else if(find == 0){      
+                action_2()
+            }	
+        } 
+        else if(move == 3){
+            if(find>0 && find < list - 1){
+                move = find;
+                action_1();
+            }
+            else if(find == 0){      
+                move = 4
+                action_2()
+            }	   
         }
+    })
+ 
+    // ====== 函式區塊 ======
+	function goTransform(page,time){
+		$('.banner_group').css('transform',`translateX(-${page * 100}%)`).css('transition',`${time}s`);
+	}
+	function changeClass(name){
+		$(`.banner_group>div`).removeClass('active')
+		$(`.banner_group>div:eq(${name-1})`).addClass('active')
     }
-})
+    function action_1(){
+        goTransform(move, 0.5);
+        setTimeout(function(){changeClass(move+1)},500);
+        $(`.banner_dots li:eq(${move})`).addClass('on')
+    }
+    function action_2(){
+        goTransform(move, 0.5);
+		setTimeout(function(){goTransform(0, 0);},500);
+        setTimeout(function(){
+            changeClass(1)
+            move = 0
+        },510)
+        $(`.banner_dots li:eq(0)`).addClass('on')
+    }
+
+    
+    // 漢堡bar點擊menu 展開/關閉
+    $('.menu_btn').click(function(){$(this).toggleClass('active')})
+    $('.menu li').click(function(){$('.menu_btn').removeClass('active')})
+    
+	var scroll = $(window).scroll(function () {
+		for (var i = 1; i < 4; i++) {
+			if (scroll.scrollTop() > $(`#hm_${i}`).offset().top + $(`#hm_${i}`).height() / 3 * 2) {
+				$(`#hm_${i + 1}`).addClass('active')
+			}
+		}
+    })
+    
